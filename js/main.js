afa79435801f711d61eacae381fa7ea9b06e4924
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-05-28 15:56:05 +0900

    JS: checked

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
   const closeBtn = document.querySelector(".close-btn");
   const logSns = document.querySelector(".log-sns");
   const bg = document.querySelector(".bg");
+  const termsElems = document.querySelectorAll(".terms-item .form-input");
 
   function closeJoinForm() {
     document.body.classList.remove("On");
@@ -11,6 +12,19 @@
     document.body.classList.add("On");
     bg.classList.remove("On");
   }
+
+  function handleTermsElems(e) {
+    console.log(e.target);
+    if (e.target.classList.contains("all")) {
+      for (let i = 0; i < termsElems.length; i++) {
+        termsElems[i].checked = true;
+      }
+    }
+  }
+
+  termsElems.forEach((elem) => {
+    elem.addEventListener("click", handleTermsElems);
+  });
 
   closeBtn.addEventListener("click", closeJoinForm);
   logSns.addEventListener("click", openJoinForm);
