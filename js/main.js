8695561a39ad9ebad12076e4d91774eeb339cc09
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-05-28 16:32:11 +0900

    complete

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
   const logSns = document.querySelector(".log-sns");
   const bg = document.querySelector(".bg");
   const termsElems = document.querySelectorAll(".terms-item .form-input");
+  const more = document.querySelectorAll(".more");
+  const moreTxtWrap = document.querySelectorAll(".terms-txt-wrap");
+  let current;
 
   function closeJoinForm() {
     document.body.classList.remove("On");
@@ -14,7 +17,6 @@
   }
 
   function handleTermsElems(e) {
-    console.log(e.target);
     if (e.target.classList.contains("all")) {
       for (let i = 0; i < termsElems.length; i++) {
         termsElems[i].checked = true;
@@ -24,6 +26,19 @@
 
   termsElems.forEach((elem) => {
     elem.addEventListener("click", handleTermsElems);
+  });
+
+  function handleViewMore(e) {
+    current = e.target.dataset.filter;
+    for (let i = 0; i < moreTxtWrap.length; i++) {
+      if (current == moreTxtWrap[i].dataset.link) {
+        moreTxtWrap[i].classList.toggle("On");
+        more[i].classList.toggle("On");
+      }
+    }
+  }
+  more.forEach((more, i) => {
+    more.addEventListener("click", handleViewMore);
   });
 
   closeBtn.addEventListener("click", closeJoinForm);
